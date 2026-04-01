3667ed70d154ccde96760e3989847ed1bd79a9a1
Ashminita <ashminitab@mindfiresolutions.com>
2026-04-01 08:52:39 +0530

    feat(service-a): add shared package to dependency of service-a

--- apps/service-a/src/server.ts
+++ apps/service-a/src/server.ts
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
+import { logger } from '@all/shared';
 
 const PORT = Number(process.env.PORT) || 3000;
 const start = async () => {
   app.listen(PORT, () => {
-    console.log('job submitter service started at port 3000');
+    logger.info('job submitter service started at port 3000');
   });
 };
 
