978ce448ba1c16af89b14e950fdd0763ee81433a
Ashminita <ashminitab@mindfiresolutions.com>
2026-04-01 16:03:05 +0530

    refactor(shared): add redis error logger

--- shared/src/configs/redis-client.ts
+++ shared/src/configs/redis-client.ts
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import Redis from 'ioredis';
+import logger from './logger';
 
 const redis = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
-  port: 6379,
+  port: Number(process.env.REDIS_PORT) || 6379,
 });
+redis.on('error', (err) => logger.error(err));
 
 export default redis;
