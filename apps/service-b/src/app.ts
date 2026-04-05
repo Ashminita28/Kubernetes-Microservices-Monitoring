8f41c2b5cee9e387b3660b3d31be32e4cac5073c
Ashminita <ashminitab@mindfiresolutions.com>
2026-04-06 00:51:38 +0530

    feat(service-b): add helmet,rate limiter,morgan and health check to service-b

--- apps/service-b/src/app.ts
+++ apps/service-b/src/app.ts
@@ -1,7 +1,20 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import metricsRouter from './routes/metrics-route';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { limiter } from '@all/shared';
 const app: Express = express();
 
+app.use(helmet());
+app.use(limiter);
+app.use(morgan('dev'));
+app.get('/health', (_req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'OK',
+    service: 'service-b-worker-service',
+    currentTime: `${new Date().toISOString()}`,
+  });
+});
 app.use(metricsRouter);
 
 export default app;
