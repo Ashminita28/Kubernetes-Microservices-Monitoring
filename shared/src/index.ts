becc89cb67dcce0a48c436f4660b11aa4b3d9087
Ashminita <ashminitab@mindfiresolutions.com>
2026-04-06 00:47:09 +0530

    feat(shared): add rate limiter config

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,4 @@
 export { redis } from './configs/redis-client';
 export { logger } from './configs/logger';
 export { REDIS_KEY } from './constants/redis';
+export { limiter } from './configs/rate-limiter';
